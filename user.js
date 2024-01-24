b73288d26dc74733aa7255438fbcc2a35faf0952
ValentayMany <valenthaiymany@gmail.com>
2024-01-24 21:48:43 +0700

    first commit

---
+++ user.js
@@ -0,0 +1,13 @@
+let sidebar = document.querySelector(".sidebar");
+let closeBtn = document.querySelector("#btn");
+let searchBtn = document.querySelector(".bx-search");
+
+closeBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("open");
+  menuBtnChange();
+});
+
+searchBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("open");
+  menuBtnChange();
+});
